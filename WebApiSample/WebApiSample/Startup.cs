907e50801dba7b05c3cef5672a3df8e98e721767
vasa911 <vadym.vasylyev@outllok.com>
2021-09-07 16:12:39 +0300

    Enable cors

---
+++ WebApiSample/WebApiSample/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebApiSample;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace WebApiSample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+
+            // Get your HttpConfiguration. In OWIN, you'll create one rather than using GlobalConfiguration.
+            var config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+        }
+    }
+}
---
+++ WebApiSample/WebApiSample/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApiSample
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.EnableCors();
+
+            System.Web.Http.GlobalConfiguration.Configuration.Routes.MapHttpRoute(
+            name: "DefaultApi",
+            routeTemplate: "api/{controller}/{action}/{id}",
+            defaults: new { id = RouteParameter.Optional });
+        }
+    }
+}
